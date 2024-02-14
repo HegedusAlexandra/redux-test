f6b92c5a2200b1e0490ea063101a7dbe80d4c262
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-02-14 16:13:14 +0100

    base

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
            <span style={{color:'white' }}>Deadlee Cereal </span>Webshop:<span style={{ color: 'white' }}> As good you would die for it</span>   
         </h1>
       </div>
+      <div className='container'></div>
     </div>
   );
 }
