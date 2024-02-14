90c4bea8dbf147678e0f5092824ee6078344d78b
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-02-14 15:45:21 +0100

    change

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="app">
       <div className="headline">
         <h1>
-           <span style={{color:'white' }}>Deadlee Cereal </span>Webshop:<span style={{ color: 'white' }}> As good you would die for it</span>
-           
+           <span style={{color:'white' }}>Deadlee Cereal </span>Webshop:<span style={{ color: 'white' }}> As good you would die for it</span>   
         </h1>
       </div>
     </div>
