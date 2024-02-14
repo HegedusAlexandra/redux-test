97c8952f6479cca67e78e360b6744b68cb0a6b9e
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-02-14 15:43:23 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <div className="headline">
         <h1>
            <span style={{color:'white' }}>Deadlee Cereal </span>Webshop:<span style={{ color: 'white' }}> As good you would die for it</span>
+           
         </h1>
       </div>
     </div>
